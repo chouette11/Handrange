4d7d713c1219ef4c73b488ac93d33ca4abd28ad4
kei1115 <keifukuda1115@gmail.com>
2021-03-24 03:38:02 +0900

    model.count -> VPIP

--- lib/main.dart
+++ lib/main.dart
@@ -86,7 +86,7 @@ class _TextFiledState extends State<TextField> {
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
           Text(
-            '${model.count}%',
+            '${(model.count / 1326) * 100}%',
             style: TextStyle(
               fontSize: 20.0,
             ),
