e35b5ddbece786013edcfa30b8ac3eb3028207f0
kei1115 <keifukuda1115@gmail.com>
2021-04-15 17:23:59 +0900

    complete sql

---
+++ lib/savepage.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:handrange/light.dart';
+
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Handrange',
+        theme: ThemeData(
+          primarySwatch: Colors.lightBlue,
+        ),
+        home: ChangeNotifierProvider<Light>(
+          create: (_) => Light(),
+
+        )
+    );
+  }
+}
