e276ecf096a9318c3387e214298d47bbfca42d23
kei1115 <keifukuda1115@gmail.com>
2021-06-03 22:36:04 +0900

    fix var

--- lib/providers/eqcalculation.dart
+++ lib/providers/eqcalculation.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/cupertino.dart';
 
 class EqCalculation extends ChangeNotifier {
-  int num1 = 0;
-  int num2 = 0;
+  int num1_1 = 0;
+  int num1_2 = 0;
+  int num2_1 = 0;
+  int num2_2 = 0;
 
-  String mark1 = "";
-  String mark2 = "";
+  String mark1_1 = "";
+  String mark1_2 = "";
+  String mark2_1 = "";
+  String mark2_2 = "";
 
-  String card1 = "";
-  String card2 = "";
-
-
+  String card1_1 = "";
+  String card1_2 = "";
+  String card2_1 = "";
+  String card2_2 = "";
 }
