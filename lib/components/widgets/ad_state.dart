723ff6add644deb8a5b7f8598fb0cbf62a743171
kei1115 <keifukuda1115@gmail.com>
2021-06-15 20:59:25 +0900

    preparing ios Ad

--- lib/components/widgets/ad_state.dart
+++ lib/components/widgets/ad_state.dart
@@ -10,30 +10,13 @@ class AdState {
 
   static String get bannerAdUnitId => Platform.isAndroid
       ? 'ca-app-pub-3443545166967285/6738424627'
-      : 'ca-app-pub-3940256099942544/6300978111';
+      : 'ca-app-pub-3443545166967285/1447892317';
+
+  static String get popAdUnitId => Platform.isAndroid
+      ? 'ca-app-pub-3443545166967285/6738424627'
+      : 'ca-app-pub-3443545166967285/5896551010';
 
   static final BannerAdListener bannerListener = BannerAdListener(
-    // Called when an ad is successfully received.
-    onAdLoaded: (Ad ad) => print('Ad loaded.'),
-    // Called when an ad request failed.
-    onAdFailedToLoad: (Ad ad, LoadAdError error) {
-      // Dispose the ad here to free resources.
-      ad.dispose();
-      print('Ad failed to load: $error');
-    },
-    // Called when an ad opens an overlay that covers the screen.
-    onAdOpened: (Ad ad) => print('Ad opened.'),
-    // Called when an ad removes an overlay that covers the screen.
-    onAdClosed: (Ad ad) => print('Ad closed.'),
-    // Called when an impression occurs on the ad.
-    onAdImpression: (Ad ad) => print('Ad impression.'),
-  );
-
-  static String get nativeAdUnitId => Platform.isAndroid
-      ? 'ca-app-pub-3940256099942544/2247696110'
-      : 'ca-app-pub-3443545166967285/8508933181';
-
-  static final NativeAdListener nativeListener = NativeAdListener(
     // Called when an ad is successfully received.
     onAdLoaded: (Ad ad) => print('Ad loaded.'),
     // Called when an ad request failed.
@@ -97,7 +80,7 @@ class _PopAdState extends State<PopAd> {
   void initState(){
     super.initState();
     _ad = BannerAd(
-      adUnitId: unitId(AdState.bannerAdUnitId),
+      adUnitId: unitId(AdState.popAdUnitId),
       size: AdSize.largeBanner,
       request: AdRequest(),
       listener: AdState.bannerListener,
