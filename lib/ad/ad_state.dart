8070811ef82f4b4281e9ae6876aabc7e78101cea
kei1115 <keifukuda1115@gmail.com>
2021-09-15 20:41:50 +0900

    Fix import

--- lib/ad/ad_state.dart
+++ lib/ad/ad_state.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-
 import 'package:flutter/cupertino.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:handrange/components/functions/elements.dart';
