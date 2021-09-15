5ea412ba69426a19213e1bbb13af44892291b561
kei1115 <keifukuda1115@gmail.com>
2021-09-15 20:38:24 +0900

    Add SavedRange

---
+++ lib/components/saved_range.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:handrange/components/widgets/gridview.dart';
+import 'package:handrange/components/widgets/tapbox.dart';
+
+import 'functions/creategraph.dart';
+
+class SavedRange extends StatelessWidget {
+  SavedRange({
+    Key? key,
+    required this.num,
+    required this.name,
+    required this.count,
+    required this.text,
+    required this.onPressed,
+  }) : super(key: key);
+
+  final int num;
+  final String name;
+  final int count;
+  final String text;
+  final void Function() onPressed;
+  final myController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPressed,
+      child: Column(
+        children: [
+          HandRange(
+            children: getTFs(text).map((e) => GridTile(
+              child: CustomTapBox(isSelected: e["isSelected"],),
+            ),
+            ).toList(),
+            size: 3.3,
+          ),
+          Center(
+            child: Column(
+              children: [
+                Text("VPIP ${((count / 1326) * 100).toStringAsFixed(2)}%"),
+                Text(name),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
