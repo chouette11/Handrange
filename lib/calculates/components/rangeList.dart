a2552d8de6317b636d6974f5b95e2dad9dc667b0
kei1115 <keifukuda1115@gmail.com>
2021-09-21 07:29:46 +0900

    Create RangeList component

---
+++ lib/calculates/components/rangeList.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:handrange/calculates/components/saved_range.dart';
+import 'package:handrange/components/functions/creategraph.dart';
+import 'package:handrange/data/sql.dart';
+
+class RangeList extends StatefulWidget {
+  RangeList({Key? key, required this.range}) : super(key: key);
+  final List<Map<String, dynamic>> range;
+  @override
+  _RangeListState createState() => _RangeListState();
+}
+
+class _RangeListState extends State<RangeList>{
+  final Future<List<Graph>> graphs = Graph.getGraph();
+
+  @override
+  Widget build(BuildContext context) {
+    double screenSizeWidth = MediaQuery.of(context).size.width;
+    return FutureBuilder(
+        future: graphs,
+        builder: (BuildContext context, AsyncSnapshot<List<Graph>> snapshot) {
+          if (snapshot.hasData) {
+            return Container(
+              margin: EdgeInsets.only(left: 2.5,right: 2.5,top: 2),
+              width: screenSizeWidth,
+              child: GridView.count(
+                crossAxisCount: 2,
+                mainAxisSpacing: 0.5,
+                crossAxisSpacing: 1,
+                childAspectRatio: 0.75,
+                children: getIds(snapshot).map((e) =>
+                    GridTile(
+                      child: SavedRange(
+                        num: e["num"],
+                        text: e["text"],
+                        name: e["name"],
+                        count: e["count"],
+                        onPressed: () {
+                          onGetRange(e['id'], widget.range, snapshot.data);
+                          Navigator.pop(context);
+                        }
+                      ),
+                    ),
+                ).toList(),
+              ),
+            );
+          }
+          else if(snapshot.hasError){
+            return Center(child: Text("Error"));
+          }
+          else{
+            return Center(child: CircularProgressIndicator());
+          }
+        }
+    );
+  }
+}
