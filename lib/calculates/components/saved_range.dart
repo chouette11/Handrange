29a16e9b09437ba6939d370f4407b8b8ad706fff
kei1115 <keifukuda1115@gmail.com>
2021-09-21 10:32:14 +0900

    Add longPress

--- lib/calculates/components/saved_range.dart
+++ lib/calculates/components/saved_range.dart
@@ -12,6 +12,7 @@ class SavedRange extends StatelessWidget {
     required this.count,
     required this.text,
     required this.onPressed,
+    this.onLongPress,
   }) : super(key: key);
 
   final int num;
@@ -19,19 +20,20 @@ class SavedRange extends StatelessWidget {
   final int count;
   final String text;
   final void Function() onPressed;
+  final void Function()? onLongPress;
   final myController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: onPressed,
+      onLongPress: onLongPress,
       child: Column(
         children: [
           HandRange(
-            children: getIsSelected(text).map((e) => GridTile(
-              child: CustomTapBox(isSelected: e["isSelected"],),
-            ),
-            ).toList(),
+            children: getIsSelected(text).map((e) =>
+                GridTile(child: CustomTapBox(isSelected: e["isSelected"]),
+            )).toList(),
             size: 3.3,
           ),
           Center(
