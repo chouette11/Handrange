128b1b34e9af25e999c6d363596d49d90661c5ef
kei1115 <keifukuda1115@gmail.com>
2021-09-28 08:15:17 +0900

    Fix cardBox UI

--- lib/calculates/components/cardbox.dart
+++ lib/calculates/components/cardbox.dart
@@ -4,32 +4,30 @@ import 'package:handrange/components/functions/elements.dart';
 
 Container cardBox(int? number, String? mark){
   return Container(
-    color: number == null || number == 0 ? Colors.black26 : Colors.white,
-    child: Container(
-        width: 40,
-        height: 65,
-        decoration: BoxDecoration(
-          border: Border.all(color: Colors.black),
-          borderRadius: BorderRadius.circular(5.0),
+    width: 48,
+    height: 72,
+    decoration: BoxDecoration(
+      color: number == null || number == 0 ? Colors.black12 : Colors.white,
+      border: number == null || number == 0 ? null : Border.all(color: Colors.black54, width: 1.6),
+      borderRadius: BorderRadius.circular(4)
+    ),
+    child: Column(
+      children: [
+        Center(
+          child: Text(
+            returnNumber(number),
+            style: TextStyle(fontSize: 24,fontFamily: "PTS",color: Colors.grey[800]),
+          ),
         ),
-        child: Column(
-          children: [
-            Center(
-              child: Text(
-                returnNumber(number),
-                style: TextStyle(fontSize: 23,fontFamily: "PTS",color: Colors.grey[800]),
-              ),
-            ),
-            Center(
-              child: Text(
-                returnMark(mark),
-                style: mark == "♠" || mark == "♣" ?
-                TextStyle(fontSize: 23,fontFamily: "PTS", color: Colors.grey[700]) :
-                TextStyle(fontSize: 23,fontFamily: "PTS", color: Colors.red[900]),
-              ),
-            ),
-          ],
+        Center(
+          child: Text(
+            returnMark(mark),
+            style: mark == "♠" || mark == "♣" ?
+            TextStyle(fontSize: 24,fontFamily: "PTS", color: Colors.grey[700]) :
+            TextStyle(fontSize: 24,fontFamily: "PTS", color: Colors.red[900]),
+          ),
         ),
+      ],
     ),
   );
 }
