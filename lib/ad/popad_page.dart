cbdb7b13ec56081da512d907d0aa914700472ac1
kei1115 <keifukuda1115@gmail.com>
2021-12-10 01:03:42 +0900

    Add PopAdPageAndroid

--- lib/ad/popad_page.dart
+++ lib/ad/popad_page.dart
@@ -104,3 +104,141 @@ class PopAdPage extends StatelessWidget {
     );
   }
 }
+
+class PopAdPageAndroid extends StatefulWidget {
+  PopAdPageAndroid({Key? key, required this.time}) : super(key: key);
+  final double time;
+
+  @override
+  _PopAdPageAndroidState createState() => _PopAdPageAndroidState();
+}
+
+class _PopAdPageAndroidState extends State<PopAdPageAndroid> {
+  bool isCalculate = false;
+  @override
+  Widget build(BuildContext context) {
+    int minute = (widget.time ~/ 60) + 1;
+    final double screenSizeWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      body:WillPopScope(
+        onWillPop: () async => false,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Consumer<EquityPageModel>(builder: (context, model, child) {
+              print(widget.time);
+              final Future<String> _calculation = Future<String>.delayed(
+                  Duration(seconds: widget.time.toInt()),
+                      () => '計算が終わりました'
+              );
+
+              return FutureBuilder(
+                  future: _calculation,
+                  builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+                    if (snapshot.hasData) {
+                      return Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(8)
+                        ),
+                        width: screenSizeWidth,
+                        height: 180,
+                        margin: EdgeInsets.only(left: 16, right: 16),
+                        child: Column(
+                          children: [
+                            Container(
+                                margin: EdgeInsets.only(top: 4),
+                                child: Text("計算が完了しました",
+                                style: TextStyle(
+                                  fontSize: 16
+                                ),)
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: RaisedButton(
+                                onPressed: () => Navigator.pushNamedAndRemoveUntil(context, '/equity', (Route<dynamic> route) => false),
+                                child: Text("表示"),
+                              ),
+                            ),
+                            Expanded(
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  BottomAd(),
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
+                      );
+                    } else {
+                      return Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(8)
+                        ),
+                        width: screenSizeWidth,
+                        height: 200,
+                        margin: EdgeInsets.only(left: 16, right: 16),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            children: [
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text("計算中です"),
+                                  Container(
+                                    margin: EdgeInsets.only(left: 4),
+                                    width: 16,
+                                    height: 16,
+                                    child: CircularProgressIndicator(strokeWidth: 1.5),
+                                  ),
+                                ],
+                              ),
+                              Column(
+                                children: [
+                                  Text("想定時間$minute分"),
+                                  Text("ご利用ありがとうございます"),
+                                  Text("不具合・改善点がありましたらお問い合わせください"),
+                                  Container(
+                                    child: InkWell(
+                                      child: Text(
+                                        "twitter_@chouette555まで",
+                                        style: TextStyle(color: Colors.lightBlue),
+                                      ),
+                                      onTap: () async {
+                                        const url = "https://twitter.com/chouette555";
+                                        if (await canLaunch(url)) {
+                                          await launch(url);
+                                        } else {
+                                          throw 'Could not Launch $url';
+                                        }
+                                      },
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Expanded(
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  children: [
+                                    BottomAd(),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    }
+                  }
+              );
+            }),
+          ],
+        ),
+      ),
+    );
+  }
+}
